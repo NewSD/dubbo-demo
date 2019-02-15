1885946fe804a6b025205639621e21d0ff97d1fd
niuhaideng <niuhaideng@163.com>
2019-02-15 19:18:53 +0800

    user serializable

--- demo-api/src/main/java/com/test/DemoService.java
+++ demo-api/src/main/java/com/test/DemoService.java
@@ -1,9 +1,16 @@
 package com.test;
 
+import org.omg.PortableInterceptor.USER_EXCEPTION;
+
+import java.util.List;
+
 /**
  * Created by ami on 2019/2/15.
  */
 public interface DemoService {
 
     String sayHello(String name);
+
+    List getUsers();
+
 }
--- dubbo-consumer/src/test/java/com/test/Test.java
+++ dubbo-consumer/src/test/java/com/test/Test.java
@@ -3,6 +3,7 @@ package com.test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by ami on 2019/2/15.
@@ -14,7 +15,19 @@ public class Test {
         context.start();
         DemoService demoService = (DemoService) context.getBean("demoService");
 
-        System.out.println(demoService.sayHello("哈哈哈"));
+        String hello = demoService.sayHello("哈哈哈");
+        System.out.println(hello);
+        //为何Exception in thread "main" com.alibaba.dubbo.rpc.RpcException:
+        // Failed to invoke the method getUsers in the service com.test.DemoService.
+        // Serialized class com.test.User must implement java.io.Serializable
+        List list = demoService.getUsers();
+        if (list != null && list.size() > 0) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+        }
+
+
         try {
             System.in.read();
         } catch (IOException e) {
